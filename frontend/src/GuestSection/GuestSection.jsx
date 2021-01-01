16d2a995dbeb6c8aebcb817df9e233a11b598805
A7abhilash <a7abhilash@gmail.com>
2021-01-01 15:01:51 +0530

    updated home page

--- frontend/src/GuestSection/GuestSection.jsx
+++ frontend/src/GuestSection/GuestSection.jsx
@@ -1,28 +1,45 @@
 import React from "react";
 import { motion } from "framer-motion";
+import blog from "./../blog.png";
+
 const BACKEND_URL = "http://localhost:7781";
 
 function GuestSection() {
   return (
     <motion.div
-      variants={guestVariant}
+      variants={guestCardVariant}
       initial="hidden"
       animate="visible"
       exit="exit"
-      className="col-md-4 text-center card bg-secondary p-0 mx-auto mt-5"
+      className="col-md-4 text-center card bg-dark p-0 mx-auto mt-5"
     >
-      <div className="card-header">
-        <h4>Blogchord</h4>
+      <div className="card-header border-bottom border-secondary py-1">
+        <motion.h3 variants={guestTextVariant}>Welcome user!</motion.h3>
       </div>
       <div className="card-content p-2">
-        <h5>Welcome user!</h5>
-        <a
+        <motion.img
+          variants={guestTextVariant}
+          src={blog}
+          alt="blog"
+          className="img-fluid mb-2"
+        />
+        <motion.h5 variants={guestTextVariant} className="text-danger">
+          Post creative blogs...
+        </motion.h5>
+        <motion.p variants={guestTextVariant}>
+          A blogging platform designed for little stories that make your day
+          better.
+        </motion.p>
+        <motion.a
+          variants={guestButtonVariant}
           role="button"
           href={`${BACKEND_URL}/auth/google`}
-          className="btn btn-sm btn-primary"
+          className="btn btn-sm btn-primary pb-0"
         >
-          Sign in
-        </a>
+          <h6>
+            <i className="fab fa-google mr-1"></i> Login with Google
+          </h6>
+        </motion.a>
       </div>
     </motion.div>
   );
@@ -30,7 +47,7 @@ function GuestSection() {
 
 export default GuestSection;
 
-const guestVariant = {
+const guestCardVariant = {
   hidden: {
     scale: 0,
   },
@@ -40,6 +57,37 @@ const guestVariant = {
   },
   exit: {
     scale: 0,
+    transition: { delay: 0.2, duration: 1, ease: "easeInOut" },
+  },
+};
+
+const guestTextVariant = {
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+    transition: { delay: 0.8, duration: 0.7 },
+  },
+  exit: {
+    opacity: 0,
+    transition: { delay: 0.1, duration: 1, ease: "easeInOut" },
+  },
+};
+
+const guestButtonVariant = {
+  hidden: {
+    opacity: 0,
+    y: "5vh",
+  },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: { delay: 1, duration: 0.7 },
+  },
+  exit: {
+    opacity: 0,
+    y: "5vh",
     transition: { delay: 0.1, duration: 1, ease: "easeInOut" },
   },
 };
